014e0c1c0ea3313cd8ac7200b707087c3a9e8a55
Gabor Koncz <gabee1987@gmail.com>
2021-05-05 16:25:27 +0200

    implemented add product to shopping cart feature

---
+++ eShop.UseCases/ViewProductScreen/AddProductToCartUseCase.cs
@@ -0,0 +1,27 @@
+﻿using eShop.UseCases.PluginInterfaces.DataStore;
+using eShop.UseCases.PluginInterfaces.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShop.UseCases.ViewProductScreen
+{
+    public class AddProductToCartUseCase : IAddProductToCartUseCase
+    {
+        private readonly IProductRepository m_productRepository;
+        private readonly IShoppingCart m_shoppingCart;
+        public AddProductToCartUseCase(
+            IProductRepository productRepository,
+            IShoppingCart shoppingCart )
+        {
+            this.m_productRepository = productRepository;
+            this.m_shoppingCart = shoppingCart;
+        }
+
+        public async void Execute( int productId )
+        {
+            var product = m_productRepository.GetProduct( productId );
+            await m_shoppingCart.AddProductAsync( product );
+        }
+    }
+}
