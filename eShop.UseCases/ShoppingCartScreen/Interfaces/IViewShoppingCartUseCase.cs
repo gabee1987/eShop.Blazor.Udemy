d6c0634d30e390a5e226ca6433284820baca8f30
Gabor Koncz <gabee1987@gmail.com>
2021-05-11 14:15:41 +0200

    implemented the shopping cart component

---
+++ eShop.UseCases/ShoppingCartScreen/Interfaces/IViewShoppingCartUseCase.cs
@@ -0,0 +1,10 @@
+﻿using eShop.CoreBusiness.Models;
+using System.Threading.Tasks;
+
+namespace eShop.UseCases.ViewShoppingCartUseCase
+{
+    public interface IViewShoppingCartUseCase
+    {
+        Task<Order> Execute();
+    }
+}
---
+++ eShop.UseCases/ShoppingCartScreen/ViewShoppingCartUseCase.cs
@@ -0,0 +1,28 @@
+﻿using eShop.CoreBusiness.Models;
+using eShop.UseCases.PluginInterfaces.UI;
+using eShop.UseCases.ViewShoppingCartUseCase;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShop.UseCases.ShoppingCartScreen
+{
+    namespace eShop.UseCases.ViewShoppingCartUseCase
+    {
+        public class ViewShoppingCartUseCase : IViewShoppingCartUseCase
+        {
+            private readonly IShoppingCart shoppingCart;
+
+            public ViewShoppingCartUseCase( IShoppingCart shoppingCart )
+            {
+                this.shoppingCart = shoppingCart;
+            }
+
+            public Task<Order> Execute()
+            {
+                return shoppingCart.GetOrderAsync();
+            }
+        }
+    }
+}
