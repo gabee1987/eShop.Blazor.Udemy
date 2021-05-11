ea6efb3b70dd0bcc3409193acb435a46a6a3f83a
Gabor Koncz <gabee1987@gmail.com>
2021-05-11 13:59:00 +0200

    implemented view shopping cart use case

---
+++ eShop.UseCases/View ShoppingCartUseCase/ViewShoppingCartUseCase.cs
@@ -0,0 +1,24 @@
+﻿using eShop.CoreBusiness.Models;
+using eShop.UseCases.PluginInterfaces.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShop.UseCases.View_ShoppingCartUseCase
+{
+    public class ViewShoppingCartUseCase
+    {
+        private readonly IShoppingCart shoppingCart;
+
+        public ViewShoppingCartUseCase( IShoppingCart shoppingCart )
+        {
+            this.shoppingCart = shoppingCart;
+        }
+
+        public Task<Order> Execute()
+        {
+            return shoppingCart.GetOrderAsync();
+        }
+    }
+}
